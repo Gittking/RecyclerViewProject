ce909c29359492f7683ebc680ad99ac938e07d94
GittKing <amuneisrael224@gmail.com>
2022-03-25 03:55:44 -0700

    added new field to movieData for display

---
+++ app/src/main/java/com/ozproject/androidfactory/MovieData2.kt
@@ -0,0 +1,8 @@
+package com.ozproject.androidfactory
+
+data class MovieData2 (
+    var imageUrl : String = "",
+    var imageResource : Int =0,
+    var isFavourite: Boolean,
+    var movieRating:String=""
+)
